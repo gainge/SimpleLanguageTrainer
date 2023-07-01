34526c71e6c6873af40de99c454abaacc91159f1
Gibson Ainge <gainge@gmail.com>
2023-07-01 12:32:40 -0600

    load lopics in script

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-
-
 // Globals (lol)
 let tools = undefined;
+let topics = undefined;
 
 
 
 fetch("res/data.json")
   .then((response) => response.json())
-  .then(json => initializeState(json['tools']));
+  .then(json => initializeState(json['tools'], json['topics']));
 
 // Then we do some cool stuff
 
-function initializeState(toolsData) {
-  if (!toolsData) {
+function initializeState(toolsData, topicsData) {
+  if (!toolsData || !topicsData) {
     alert('unable to retrieve json data!');
   }
   tools = toolsData;
+  topics = topicsData
 }
